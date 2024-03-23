29643acf83e5059911cebcaa534c568aabd2d0d1
joigfema <josignfermam@gmail.com>
2024-03-23 16:19:27 -0400

    feat: add my task

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css'
 import './src/hello'
 import './src/homework'
-
+//con que importanto homework he ejejje
 document.querySelector('#app').innerHTML = `
   <div>
     <h2>LABO - 03</h2>
--- src/homework.js
+++ src/homework.js
@@ -1,54 +1,46 @@
 (() => {
-
-  /* Excercises */
-
-  /* First excercise */
+/* first excercise */
   
   let numbers = [1, 2, 3, 4, 5];
   
-  /* Multiplique los numeros del arreglo por 5 */
-
-  /*Resultado esperado: [5, 10, 15, 20, 25] */
-
-  /* First excercise */
-
+  for(let i=0; i < numbers.length; i++ ){
+    numbers[i]*=5;
+  }
+  numbers.forEach((Element, index) => {
+    console.log("el elemento es: ", Element, "con indice de: ", index)
+  });
+ 
   /* Second excercise */
+    /* names.sort((a,b)=>{
+      return a.localeCompare(b);
+    });
+    console.log(names);
+    ya entendi a los callback y como funciona la funcion flecha, esta cosa no es necesaria
+    */
+    let names = ["Jhosep", "Daniel", "Rodrigo", "Veronica"];
+    console.log(names.sort());
 
-  let names = ["Jhosep", "Daniel", "Rodrigo", "Veronica"];
-
-  /* Ordernar los nombres alfabeticamente */
-
-  /* Resultado esperado: ["Daniel", "Jhosep", "Rodrigo", "Veronica"] */
-
-  /* Second excercise */
 
   /* Thrid excercise */
 
   const data = ['a', 'b', 'z', 'r', 'a', 'c', 'r', 'a'];
-
-  /* Dado un arreglo, crear otro sin letras repetidas */
-  /* Resultado esperado: ['a', 'b', 'z', 'r', 'c'] */
-
-  /* Thrid excercise */
-
-  /* Fourth excercise */
-
-  const largeWord = "abcdabcdABCD";
-
-  /* Dado un string mostrar cuantas veces se repite cada letra*/
-  /* Resultado: 
-    {
-      a: 2,
-      b: 2,
-      c: 2,
-      d: 2,
-      A: 1,
-      B: 1,
-      C: 1,
-      D: 1
+  data.sort((a, b)=> a.localeCompare(b));
+  let UniqueData = new Set(data);
+  console.log(UniqueData);
+  
+    /* Fourth excercise */
+/* letterFrequency funciona como un diccionario de python */
+    const largeWord = "abcdabcdABCD";
+    const letterFrequency = {};
+    for (let i = 0; i < largeWord.length; i++) {
+        const letter = largeWord[i];
+        letterFrequency[letter] !== undefined
+          ? letterFrequency[letter] +=1
+          : letterFrequency[letter] = 1;
     }
-  */
-
-  /* Fourth excercise */
+    
+    console.log(letterFrequency);
+    
+  
 
 })();
